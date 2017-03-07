ea53d9d2485a82ed738199edc82cf5f79593f733
Joshua Kairu <joshuawk@yandex.com>
2017-03-07 15:57:46 +0300

    feat: Add a second activity

---
+++ app/src/main/java/com/joslittho/sampler/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.joslittho.sampler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The second activity
+ */
+// begin activity SecondActivity
+public class SecondActivity extends AppCompatActivity {
+
+    /* CONSTANTS */
+    
+    /* Integers */
+    
+    /* Strings */
+
+    /**
+     * The logger.
+     */
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    
+    /* VARIABLES */
+    
+    /* CONSTRUCTOR */
+    
+    /* METHODS */
+    
+    /* Getters and Setters */
+    
+    /* Overrides */
+
+    @Override
+    // begin onCreate
+    protected void onCreate( Bundle savedInstanceState ) {
+
+        // 0. super stuff
+
+        // 0. super stuff
+
+        super.onCreate( savedInstanceState );
+
+    } // end onCreate
+    
+    /* Other Methods */
+
+} // end activity SecondActivity
